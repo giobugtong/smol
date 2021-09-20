b38ed5afa78801d67e4960888779d250dcb0cdd1
Gio Bugtong <nlbugtong@gmail.com>
2021-09-21 04:05:18 +0800

    Add QR code generator form submission  and viewing

--- src/components/SmolLink.js
+++ src/components/SmolLink.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Button, Col, Form, FormControl, FormGroup, InputGroup, Row, Spinner} from "react-bootstrap";
+import { Button, Col, Form, FormControl, FormGroup, FormLabel, InputGroup, Modal, Row, Spinner} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
@@ -12,6 +12,12 @@ export default function SmolLink (props) {
     const [isActive, setIsActive] = useState(null);
     const [dateCreated, setDateCreated] = useState("");
     const [urlNickname, setUrlNickName] = useState("");
+
+    const [qrCode, setQrCode] = useState("");
+    const [showQrCode, setShowQrCode] = useState(false);
+    const [qrCodeSize, setQrCodeSize] = useState("");
+
+    const [initUrlNickname, setInitUrlNickname] = useState("");
     const [showInput, setShowInput] = useState("d-none");
     const [showSpinner, setShowSpinner] = useState("d-none");
     const [showBigSpinner, setShowBigSpinner] = useState("d-none");
@@ -50,6 +56,8 @@ export default function SmolLink (props) {
                     setIsActive(data.isActive);
                     setDateCreated(data.dateCreated.slice(0, 10));
                     setUrlNickName(data.urlNickname);
+                    setInitUrlNickname(data.urlNickname);
+                    setQrCode(data.qrCode);
                     setShowSpinner("d-none");
                     setShowBigSpinner("d-none");
                     setShowInput("d-none");
@@ -148,6 +156,7 @@ export default function SmolLink (props) {
                     icon: "error"
                 })
             } else {
+                setInitUrlNickname(urlNickname);
                 const Toast = Swal.mixin({
                     toast: true,
                     icon: "success",
@@ -172,9 +181,71 @@ export default function SmolLink (props) {
         })
     }
 
-    const hideInput = () => {
-        setShowInput("d-none");
-        fetchUserLinks();
+    const toggleQrCode = () => {
+        showQrCode ? setShowQrCode(false) : setShowQrCode(true);
+    }
+
+    const generateQrCode = e => {
+        e.preventDefault();
+        if (qrCode) {
+            return
+        } else {
+            fetch(`https://api.qrserver.com/v1/create-qr-code/?size=${qrCodeSize}x${qrCodeSize}&data=https://sm-ol.vercel.app/${shortUrl}`)
+            .then(result => {
+                setQrCode(result.url);
+            })
+
+            fetch(`${process.env.REACT_APP_API_URL}/links/set-qr-code`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("accessToken")}`
+                },
+                body: JSON.stringify({
+                    linkId: linkId,
+                    qrCode: qrCode
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    Swal.fire({
+                        title: "Error!",
+                        text: data.error.name,
+                        icon: "error"
+                    })
+                } else if (data.qrCodeSaved) {
+                    setInitUrlNickname(urlNickname);
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        icon: "success",
+                        position: "top",
+                        showConfirmButton: false,
+                        timer: 2000,
+                        timerProgressBar: true,
+                        showCloseButton: true
+                      })
+                    Toast.fire({
+                        text: `QR Code created!`
+                    })
+                    fetchUserLinks();
+                }
+            })
+            .catch(err => {
+                Swal.fire({
+                    title: "Error!",
+                    text: err,
+                    icon: "error"
+                })
+            })
+
+        }
+    }
+
+    const toggleNicknameInput = () => {
+        !showInput ? setShowInput("d-none") :
+        setShowInput("");
+        setUrlNickName(initUrlNickname);
     }
 
     useEffect(() => {
@@ -196,14 +267,14 @@ export default function SmolLink (props) {
         </div>
         { showBigSpinner ? 
             <Row className="bg-light py-3 mx-sm-0 border smol-container" style={{minWidth: "267px"}}>
-                <Col xs={12} className={!showInput && "d-none"}>{urlNickname ? <h5 className="overflow-auto py-1">{urlNickname}</h5> : <Link className="text-dark" target="_blank" to={`/${shortUrl}`}><h5>sm-ol.vercel.app/{shortUrl}</h5></Link>}</Col>
-                <Col xs={12} className={showInput}>
+                <Col xs={12} className={!showInput && "d-none"}>{urlNickname ? <h5 className="overflow-auto pt-2 pb-1 font-weight-bold">{urlNickname}</h5> : <Link className="text-dark" target="_blank" to={`/${shortUrl}`}><h5>sm-ol.vercel.app/{shortUrl}</h5></Link>}</Col>
+                <Col xs={12} className={`${showInput} mb-1`}>
                 <Form onSubmit={e => changeUrlNickname(e)}>
                     <InputGroup>
-                        <FormControl placeholder="Enter nickname" type="text" value={urlNickname} onChange={e => setUrlNickName(e.target.value)} className="text-dark" />
+                        <FormControl placeholder="Enter nickname" type="text" value={urlNickname} onChange={e => setUrlNickName(e.target.value)} className="text-dark font-weight-bold" style={{fontSize: "20px"}}/>
                         <InputGroup.Append>
                             <Button className="px-3 border-dark border" id="" variant="light" disabled={!showSpinner} onClick={e => changeUrlNickname(e)} size="sm">{!showSpinner ? <Spinner className={`mb-1 ml-2 ${showSpinner}`} as="span" animation="border" role="status" aria-hidden="true" size="sm" /> : <span>&#10003;</span>}</Button>
-                            <Button className="px-3 border-dark border" id="" variant="light" onClick={() => hideInput()} size="sm">&#x2715;</Button>
+                            <Button className="px-3 border-dark border" id="" variant="light" onClick={() => toggleNicknameInput()} size="sm">&#x2715;</Button>
                         </InputGroup.Append>
                     </InputGroup>
                 </Form>
@@ -211,18 +282,53 @@ export default function SmolLink (props) {
                 <Col xs={12}><span>{urlNickname ? <Link target="_blank" to={`/${shortUrl}`}>sm-ol.vercel.app/{shortUrl}</Link>  : null}</span></Col>
                 <Col xs={12} >Created: {dateCreated}</Col>
                 <Col xs={12} >Destination: </Col>
-                <Col xs={12}><div className="my-2 border py-1 px-2 longUrl-text">{longUrl}</div></Col>
+                <Col xs={12}><div className="my-2 mb-md-3 border py-1 px-2 longUrl-text">{longUrl}</div></Col>
                 <Col sm={8} >Status: <span className={`font-weight-bold ${isActive ? "text-success" : "text-danger"}`}>{isActive ? "Active" : "Deactivated"}</span></Col>
                 <Col sm={4} >Hits: {numberOfHits}</Col>
                 <Col xs={12} className="mt-3 d-block" >
-                    <Button className="themeColor my-1 link-btn" size="sm" disabled={!showSpinner} onClick={() => showInput ? setShowInput("") : setShowInput("d-none")}>{urlNickname ? "Edit nickname" : "Set nickname"}<Spinner className={`mb-1 ml-2 ${showSpinner}`} as="span" animation="border" role="status" aria-hidden="true" size="sm" /></Button>
+                    <Button className="themeColor my-1 link-btn" size="sm" disabled={!showSpinner} onClick={() => toggleNicknameInput()}>{urlNickname ? "Edit nickname" : "Set nickname"}<Spinner className={`mb-1 ml-2 ${showSpinner}`} as="span" animation="border" role="status" aria-hidden="true" size="sm" /></Button>
                     <Button className="themeColor my-1 link-btn" size="sm" disabled={!showSpinner} onClick={() => toggleStatus()}>{isActive ? "Deactivate" : "Activate"}<Spinner className={`mb-1 ml-2 ${showSpinner}`} as="span" animation="border" role="status" aria-hidden="true" size="sm" /></Button>
                     <Button className="themeColor my-1 link-btn" size="sm" onClick={() => copyLink()}>Copy link</Button>
+                    <Button className="themeColor my-1 link-btn" size="sm" onClick={() => toggleQrCode()}>{qrCode ? "View" : "Generate"} QR Code</Button>
                 </Col>
             </Row>
          :
         null
         }
+
+        <Modal show={showQrCode} onHide={() => setShowQrCode(false)} className="d-flex justify-ceontent-center align-items-center">
+            
+            <Modal.Header closeButton>
+                <Modal.Title>QR Code for {urlNickname ? urlNickname : `sm-ol.vercel.app/${shortUrl}`}</Modal.Title>
+            </Modal.Header>
+            {
+                !qrCode &&
+                <Modal.Body>
+                        <Form onSubmit={e => generateQrCode(e)}>
+                            <Form.Group>
+                                <Form.Label>Size in pixels</Form.Label>
+                                <Form.Control as="select" custom onChange={e => setQrCodeSize(e.target.value)}>
+                                    <option value="150" selected>150 x 150</option>
+                                    <option value="250">250 x 250</option>
+                                    <option value="400">400 x 400</option>
+                                    <option value="700">700 x 700</option>
+                                    <option value="1000">1000 x 1000</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Button type="submit" className="btn-block my-3 themeColor">Generate</Button>
+                        </Form>
+                </Modal.Body>
+            }
+
+            {
+                qrCode &&
+                <Modal.Body>
+                    <a href={qrCode} download={urlNickname ? `smol-qr-${urlNickname}.png` : `smol-qr-${shortUrl}.png`}>
+                        <img alt={urlNickname ? `QR Code for ${urlNickname}` : `QR Code for sm-ol.vercel.app/${shortUrl}`} fluid="true" width="200px" src={qrCode} className="d-block mx-auto"/>
+                    </a>
+                </Modal.Body>
+            }
+        </Modal>
         </>
     )
 }
